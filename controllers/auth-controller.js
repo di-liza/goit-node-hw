923ae112c0214e12dc3512676c8cc3b76c2da944
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-06 19:07:30 +0300

    fix logout

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204);
+  res.status(204).json();
 };
 
 const getCurrent = async (req, res) => {
