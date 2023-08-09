d4fa62cdaf0cd32fd3da2a163d859604f30d97aa
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-09 19:20:09 +0300

    add tests

---
+++ routes/api/auth/authRouter.test.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+import { app } from "../../../app.js";
+import User from "../../../models/user.js";
+
+const { PORT, DB_HOST } = process.env;
+
+describe("test login route", () => {
+  let server = null;
+
+  beforeAll(async () => {
+    await mongoose.connect(DB_HOST);
+    server = app.listen(PORT);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+  });
+
+  //   afterEach(async () => {
+  //     await User.deleteMany({});
+  //   });
+
+  it("test login with correct data", async () => {
+    const loginData = {
+      email: "liza.diachenko.99@gmail.com",
+      password: "_DM7WIx5nI51GjM",
+    };
+
+    const { statusCode, body } = await request(app)
+      .post("/users/login")
+      .send(loginData);
+
+    expect(statusCode).toBe(200);
+    expect(body.token).toBeDefined();
+
+    const user = await User.findOne({ email: loginData.email });
+   expect(user).toBeDefined();
+   expect(user.email).toBe(loginData.email);
+   expect(user.subscription).toBe("starter");
+  });
+});
